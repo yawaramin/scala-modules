ab2cee8167f5a97602c75c13485e24ab988b3b00
Yawar Amin <yawar.amin@gmail.com>
2015-09-23 00:15:36 -0400

    talk: implement modular style
    Also rearrange script into separate objects (modules) for the shared
    code and the inheritance-based, typeclass-based, and modular
    implementations.
    
    Trait companion objects are proving to be a perfect place to stash
    functors that create modules that ascribe to the trait signature.
    
    Need to find a way to make the typeclass-based implementation
    spiritually similar to the modular one.

--- talk.scala
+++ talk.scala
@@ -10,99 +10,162 @@ object Common {
 
 import Common._
 
-// Inheritance
+object Inheretic {
+  trait Loggable { def log(msg: String): Unit }
+  trait Clickable { def click(coords: Coords): Unit }
 
-/*
-trait Clickable {
-  def click(coords: Coords): Unit
-}
-
-trait Draggable extends Clickable {
-  def drag(startCoords: Coords, endCoords: Coords): Unit =
-    click(startCoords)
-}
-
-trait Loggable {
-  def log(msg: String): Unit
-}
-
-class GuiIcon(text: String)
-  extends Clickable with Draggable with Loggable {
-  override def click(coords: Coords) = {
-    // ...
-    log("Clicked")
+  trait Draggable extends Clickable {
+    def drag(startCoords: Coords, endCoords: Coords): Unit =
+      click(startCoords)
   }
 
-  override def drag(startCoords: Coords, endCoords: Coords) = {
-    super.drag(startCoords, endCoords)
+  class GuiIcon(text: String)
+    extends Clickable with Draggable with Loggable {
+    override def log(msg: String) = {
+      print(text)
+      print(": ")
+      println(msg)
+    }
 
-    // ...
-    log("Dragged")
+    override def click(coords: Coords) = {
+      // ...
+      log("Clicked")
+    }
+
+    override def drag(startCoords: Coords, endCoords: Coords) = {
+      super.drag(startCoords, endCoords)
+
+      // ...
+      log("Dragged")
+    }
   }
 
-  override def log(msg: String) = {
-    print(text)
-    print(": ")
-    println(msg)
+  def dragThenClick(guiIcon: GuiIcon) = {
+    guiIcon.drag(0 -> 0, 1 -> 1)
+    guiIcon.click(1 -> 1)
   }
 }
-*/
 
-// Typeclasses
+object Typeclassy {
+  trait Loggable[A] { def log(msg: String)(a: A): Unit }
+  trait Clickable[A] { def click(coords: Coords)(a: A): Unit }
 
-trait Clickable[A] {
-  def click(coords: Coords)(a: A): Unit
-}
+  trait Draggable[A] extends Clickable[A] {
+    def drag(startCoords: Coords, endCoords: Coords)(a: A): Unit =
+      a |> click(startCoords)
+  }
 
-trait Draggable[A] extends Clickable[A] {
-  def drag(startCoords: Coords, endCoords: Coords)(a: A): Unit =
-    a |> click(startCoords)
-}
+  case class GuiIcon(text: String)
 
-trait Loggable[A] {
-  def log(msg: String)(a: A): Unit
-}
-
-case class GuiIcon(text: String)
-
-object GuiIcon {
-  implicit val loggableGuiIcon: Loggable[GuiIcon] =
-    new Loggable[GuiIcon] {
-      override def log(msg: String)(guiIcon: GuiIcon) = {
-        print(guiIcon.text)
-        print(": ")
-        println(msg)
+  object GuiIcon {
+    implicit val loggableGuiIcon: Loggable[GuiIcon] =
+      new Loggable[GuiIcon] {
+        override def log(msg: String)(guiIcon: GuiIcon) = {
+          print(guiIcon.text)
+          print(": ")
+          println(msg)
+        }
       }
-    }
 
-  implicit val clickableGuiIcon: Clickable[GuiIcon] =
-    new Clickable[GuiIcon] {
-      override def click(coords: Coords)(guiIcon: GuiIcon) = {
-        // ...
-        guiIcon |> loggableGuiIcon.log("Clicked")
+    implicit val clickableGuiIcon: Clickable[GuiIcon] =
+      new Clickable[GuiIcon] {
+        override def click(coords: Coords)(guiIcon: GuiIcon) = {
+          // ...
+          guiIcon |> loggableGuiIcon.log("Clicked")
+        }
       }
-    }
 
-  implicit val draggableGuiIcon: Draggable[GuiIcon] =
-    new Draggable[GuiIcon] {
-      override def click(coords: Coords)(guiIcon: GuiIcon) =
-        guiIcon |> clickableGuiIcon.click(coords)
+    implicit val draggableGuiIcon: Draggable[GuiIcon] =
+      new Draggable[GuiIcon] {
+        override def click(coords: Coords)(guiIcon: GuiIcon) =
+          guiIcon |> clickableGuiIcon.click(coords)
 
-      override def drag(
-        startCoords: Coords, endCoords: Coords)(guiIcon: GuiIcon) = {
-        guiIcon |> super.drag(startCoords, endCoords)
+        override def drag(
+          startCoords: Coords, endCoords: Coords)(guiIcon: GuiIcon) = {
+          guiIcon |> super.drag(startCoords, endCoords)
 
-        // ...
-        guiIcon |> loggableGuiIcon.log("Dragged")
+          // ...
+          guiIcon |> loggableGuiIcon.log("Dragged")
+        }
       }
-    }
-}
+  }
 
-object TypeclassesTest {
   def dragThenClick(
     guiIcon: GuiIcon)(implicit DG: Draggable[GuiIcon]) = {
     guiIcon |> DG.drag(0 -> 0, 1 -> 1)
     guiIcon |> DG.click(1 -> 1)
+  }
+}
+
+object Modular {
+  trait Loggable[A] {
+    type T = A
+    def log(msg: String)(t: T): Unit
+  }
+
+  trait Clickable[A] {
+    type T = A
+    def click(coords: Coords)(t: T): Unit
+  }
+
+  object Clickable {
+    def Mk[A](LA: Loggable[A]): Clickable[A] =
+      new Clickable[A] {
+        override def click(coords: Coords)(t: T) = {
+          // ...
+          t |> LA.log("Clicked")
+        }
+      }
+  }
+
+  trait Draggable[A] {
+    type T = A
+    def drag(startCoords: Coords, endCoords: Coords)(t: T): Unit
+  }
+
+  object Draggable {
+    def Mk[A](LA: Loggable[A], CA: Clickable[A]): Draggable[A] =
+      new Draggable[A] {
+        override def drag(
+          startCoords: Coords, endCoords: Coords)(t: T) = {
+          t |> CA.click(startCoords)
+
+          // ...
+          t |> LA.log("Dragged")
+        }
+      }
+  }
+
+  trait GuiAble[A] {
+    type T = A
+    def dragThenClick(t: T): Unit
+  }
+
+  object GuiAble {
+    def Mk[A](CA: Clickable[A], DA: Draggable[A]): GuiAble[A] =
+      new GuiAble[A] {
+        override def dragThenClick(t: T) = {
+          t |> DA.drag(0 -> 0, 1 -> 1)
+          t |> CA.click(1 -> 1)
+        }
+      }
+  }
+
+  case class GuiIcon(text: String)
+
+  object GuiIcon {
+    val loggable: Loggable[GuiIcon] =
+      new Loggable[GuiIcon] {
+        override def log(msg: String)(t: T) = {
+          print(t.text)
+          print(": ")
+          println(msg)
+        }
+      }
+
+    val clickable = Clickable.Mk(loggable)
+    val draggable = Draggable.Mk(loggable, clickable)
+    val guiable = GuiAble.Mk(clickable, draggable)
   }
 }
 
